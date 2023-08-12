08e91de2c62a5c3922c5d6d72de8152c693c7a37
Daniel Torres <65200948+DanndxFull@users.noreply.github.com>
2023-08-12 18:39:01 -0500

    Timer transition

--- Assets/Scripts/RandomTransition.cs
+++ Assets/Scripts/RandomTransition.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+
 public class RandomTransition : MonoBehaviour
 {
     [SerializeField] private float timeToChange;
@@ -14,22 +15,73 @@ public class RandomTransition : MonoBehaviour
 
     [SerializeField] private MultipleTargetCamera multiple;
 
+    [SerializeField] private int view = 0;
+
     // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime;
-        //playTime += Time.deltaTime;
+        playTime += Time.deltaTime;
         if (currentTime > timeToChange)
         {
-            TransitionGame();
+            switch (view)
+            {
+                case 0:
+                    {
+                        TransitionGameFIFA();
+                        view = 1;
+                        break;
+                    }
+                case 1:
+                    {
+                        TransitionFrontal();
+                        view = 2;
+                        break;
+                    }
+                case 2:
+                    {
+                        TransitionGameFIFA();
+                        view = 3;
+                        break;
+                    }
+                case 3:
+                    {
+                        TransitionCenital();
+                        view = 0;
+                        break;
+                    }
+            }
             currentTime = 0;
-            timeToChange -= 15;
+        }
+        if (playTime > 34)
+        {
+            timeToChange = 10;
+        }
+        if (playTime > 94)
+        {
+            timeToChange = 8;
+        }
+        if (playTime > 116)
+        {
+            timeToChange = 6;
         }
     }
 
-    public void TransitionGame()
+    public void TransitionCenital()
+    {
+        cameraHoolder.DORotate(new Vector3(45, 0, 0), 2).OnComplete(() => cam.orthographic = true);
+        multiple.NewOffset(0, 8, 0);
+    }
+
+
+    public void TransitionGameFIFA()
+    {
+        cameraHoolder.DORotate(new Vector3(0, 0, 0), 2).OnComplete(() => cam.orthographic = false);
+        multiple.NewOffset(0, 8, -10);
+    }
+    public void TransitionFrontal()
     {
         cameraHoolder.DORotate(new Vector3(-45, 0, 0), 2).OnComplete(() => cam.orthographic = true);
-        multiple.NewOffset(0,1,-40);
+        multiple.NewOffset(0, 1, -40);
     }
 }
