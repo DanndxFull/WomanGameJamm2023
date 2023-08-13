eb67cee8279fa74bc10606f8f3f2618d9707e09c
Daniel Torres <65200948+DanndxFull@users.noreply.github.com>
2023-08-12 22:38:05 -0500

    Merge ramita alex

--- Assets/Scripts/MainPanel.cs
+++ Assets/Scripts/MainPanel.cs
@@ -6,51 +6,60 @@ using UnityEngine.Audio;
 
 public class MainPanel : MonoBehaviour
 {
-[Header("Options")]
-public Slider volumeFX;
-public Slider volumeMaster;
-public Toggle mute;
-public AudioMixer mixer;
-public AudioSource fxSource;
-public AudioClip clickSound;
-private float lastVolume;
+    [Header("Options")]
+    public Slider volumeFX;
+    public Slider volumeMaster;
+    public Toggle mute;
+    public AudioMixer mixer;
+    public AudioSource fxSource;
+    public AudioClip clickSound;
+    private float lastVolume;
 
-[Header("Panels")]
-public GameObject mainPanel;
-public GameObject optionsPanel;
+    [Header("Panels")]
+    public GameObject mainPanel;
+    public GameObject optionsPanel;
 
-private void Awake(){
-    volumeFX.onValueChanged.AddListener(ChangeVolumeFX);
-    volumeMaster.onValueChanged.AddListener(ChangeVolumeMaster);
-}
-public void SetMute(){
-    if(mute.isOn){
-        mixer.GetFloat("volMaster", out lastVolume);
-        mixer.SetFloat("volMaster", -80);
-    }else{
-        mixer.SetFloat("volMaster", lastVolume);
+    private void Awake()
+    {
+        volumeFX.onValueChanged.AddListener(ChangeVolumeFX);
+        volumeMaster.onValueChanged.AddListener(ChangeVolumeMaster);
     }
-}
-public void OpenPanel(GameObject panel){
-    mainPanel.SetActive(false);
-    optionsPanel.SetActive(false);
+    public void SetMute()
+    {
+        if (mute.isOn)
+        {
+            mixer.GetFloat("volMaster", out lastVolume);
+            mixer.SetFloat("volMaster", -80);
+        }
+        else
+        {
+            mixer.SetFloat("volMaster", lastVolume);
+        }
+    }
+    public void OpenPanel(GameObject panel)
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(false);
 
-    panel.SetActive(true);
-    PlaySoundButton();
-}
+        panel.SetActive(true);
+        PlaySoundButton();
+    }
 
-public void ChangeVolumeMaster(float v){
-    mixer.SetFloat("VolMaster", v);
-}
+    public void ChangeVolumeMaster(float v)
+    {
+        mixer.SetFloat("VolMaster", v);
+    }
 
-public void ChangeVolumeFX(float v){
-    mixer.SetFloat("VolFX", v);
-}
-public void PlaySoundButton()
-{
-fxSource.PlayOneShot(clickSound);
-}
-public void ExitGame(){
-    Application.Quit();
-}
+    public void ChangeVolumeFX(float v)
+    {
+        mixer.SetFloat("VolFX", v);
+    }
+    public void PlaySoundButton()
+    {
+        fxSource.PlayOneShot(clickSound);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
